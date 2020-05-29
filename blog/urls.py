28f02979f89a3bae00995c39eee11c80d14ca996
yukitotora <65648799+yukitotora@users.noreply.github.com>
2020-05-29 16:28:15 +0900

    コメント機能の実装

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostList.as_view(), name='post_list'),
     path('detail/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
-
+    path('comment/<int:pk>/', views.CommentCreate.as_view(), name='comment_create'),
   ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
 from django.views import generic
-from .models import Post
+from .forms import CommentCreateForm
+from .models import Post, Comment
 
 class PostList(generic.ListView):
     model = Post
@@ -7,3 +10,15 @@ class PostList(generic.ListView):
 
 class PostDetail(generic.DetailView):
     model = Post
+
+class CommentCreate(generic.CreateView):
+    model = Comment
+    form_class = CommentCreateForm
+
+    def form_valid(self, form):
+        post_pk = self.kwargs['pk']
+        post = get_object_or_404(Post, pk=post_pk)
+        comment = form.save(commit=False)
+        comment.target = post
+        comment.save()
+        return redirect('blog:post_detail', pk=post_pk)
