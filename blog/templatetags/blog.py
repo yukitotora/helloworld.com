39a2295cfe3268ae1ef67045f898b98f9f4400ce
yukitotora <65648799+yukitotora@users.noreply.github.com>
2020-06-01 15:37:52 +0900

    検索フォームの実装

--- blog/templatetags/blog.py
+++ blog/templatetags/blog.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Category, Tag
+from blog.forms import PostSearchForm
 
 register = template.Library()
 
@@ -9,3 +10,8 @@ def create_category_and_tag_list():
         'category_list': Category.objects.all(),
         'tag_list': Tag.objects.all(),
     }
+
+@register.inclusion_tag('blog/search_form.html')
+def create_search_form(request):
+    form = PostSearchForm(request.GET or None)
+    return {'form': form}
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import CommentCreateForm
+from django.db.models import Q
+from .forms import CommentCreateForm, PostSearchForm
 from .models import Post, Comment, Category, Tag
 
 class PostList(generic.ListView):
     model = Post
     ordering = '-created_at'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        form = PostSearchForm(self.request.GET or None)
+        if form.is_valid():
+            key_word = form.cleaned_data.get('key_word')
+            if key_word:
+                queryset = queryset.filter(Q(title__icontains=key_word) | Q(text__icontains=key_word))
+
+            category = form.cleaned_data.get('category')
+            if category:
+                queryset = queryset.filter(category=category)
+
+            tags = form.cleaned_data.get('tags')
+            if tags:
+                queryset = queryset.filter(tags__in=tags).distinct()
+
+            user = form.cleaned_data.get('user')
+            if user:
+                queryset = queryset.filter(writer=user)
+
+        return queryset
+
 
 class PostDetail(generic.DetailView):
     model = Post
